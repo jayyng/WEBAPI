c4f6d1fea485e496af6d756b3086aa6ae9822f7f
jayyng <41092247+jayyng@users.noreply.github.com>
2019-11-27 08:44:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apikey = 'f7467abc'; //movie api
 //--- heroku ---
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
